bba633db55f75b5e4e2199cdca87135386af851f
Om Preetham Bandi <86243960+OmPreetham@users.noreply.github.com>
2025-02-16 12:05:33 -0600

    fixed for client site

---
+++ components/ClientMap.tsx
@@ -0,0 +1,9 @@
+"use client"
+import "leaflet/dist/leaflet.css"
+
+// ... (rest of the Map component code, exactly as it was in the previous Map.tsx)
+
+export default function ClientMap() {
+  // ... (all the code from the previous Map component)
+}
+
